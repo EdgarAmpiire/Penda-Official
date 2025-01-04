95769ee103d97148a6a232213e79340299ab3ffc
timothydutki <timart575@gmail.com>
2025-01-04 17:48:55 +0300

    new updates

---
+++ build/precache-manifest.acd8170cfd3f2f46f75fcf50ff151c03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "af8bcf98fd1576121828",
+    "url": "/demo/react/abstrak/static/css/main.17f6511d.chunk.css"
+  },
+  {
+    "revision": "af8bcf98fd1576121828",
+    "url": "/demo/react/abstrak/static/js/main.94d4c2e5.chunk.js"
+  },
+  {
+    "revision": "7c6b4678392a0d5c0825",
+    "url": "/demo/react/abstrak/static/js/runtime~main.b058a727.js"
+  },
+  {
+    "revision": "bbf342506a93485c8c56",
+    "url": "/demo/react/abstrak/static/css/2.a44adb9f.chunk.css"
+  },
+  {
+    "revision": "bbf342506a93485c8c56",
+    "url": "/demo/react/abstrak/static/js/2.08439efa.chunk.js"
+  },
+  {
+    "revision": "3e1785ef074e6d621e64",
+    "url": "/demo/react/abstrak/static/js/3.09116e02.chunk.js"
+  },
+  {
+    "revision": "bad7e275eeaf5c25002b3ac5b7b85629",
+    "url": "/demo/react/abstrak/index.html"
+  }
+];
