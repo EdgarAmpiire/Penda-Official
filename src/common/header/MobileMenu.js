207b4e199a193df524e240e633f10227fb7d335f
timothydutki <timart575@gmail.com>
2025-01-15 13:15:25 +0300

    updates

--- src/common/header/MobileMenu.js
+++ src/common/header/MobileMenu.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaTimes } from "react-icons/fa";
 import Nav from './Nav';
+import {
+    FaFacebookF,
+    FaLinkedinIn,
+  //   FaBehance,
+    FaPhone,
+  //   FaFax,
+    FaInstagram,
+    FaYoutube,
+  } from "react-icons/fa";
 
 const MobileMenu = ({MobileHandler}) => {
 
@@ -19,6 +28,54 @@ const MobileMenu = ({MobileHandler}) => {
                 </div>
                 <div className="mobilemenu-body">
                     <Nav />
+                    
+                    <div className="col-lg-7 col-xl-6">
+                  <div className="contact-info-wrap">
+                    <div className="contact-inner">
+                      <address className="address">
+                        <span className="title">Contact Information</span>
+                        <p>
+                          Plot 21, Kaduyu Lane, Kiwatule <br /> Kampala, Uganda
+                        </p>
+                      </address>
+                      <address className="address">
+                        <span className="title">We're Available 24/7. Call Now.</span>
+                        <a href="tel:0775408295" className="tel">
+                          <FaPhone /> +256 775 408295
+                        </a>
+                        {/* <a href="tel:12125553333" className="tel"><FaFax /> (121) 255-53333</a> */}
+                      </address>
+                    </div>
+                    <div className="contact-inner">
+                      <h5 className="title">Find us here</h5>
+                      <div className="contact-social-share">
+                        <ul className="social-share list-style">
+                          <li>
+                            <a href="https://www.facebook.com/PendaAgency/">
+                              <FaFacebookF />
+                            </a>
+                          </li>
+      
+                          <li>
+                            <a href="https://ug.linkedin.com/company/pendaagency">
+                              <FaLinkedinIn />
+                            </a>
+                          </li>
+                          <li>
+                            <a href="https://www.instagram.com/pendaagency/">
+                              <FaInstagram />
+                            </a>
+                          </li>
+                          <li>
+                            <a href="https://www.youtube.com/channel/UCKEH2J6WhRLz9w7QB5foiuw">
+                              <FaYoutube />
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div> 
                 </div>
             </div>
         </div>
--- src/common/header/Nav.js
+++ src/common/header/Nav.js
@@ -1,13 +1,30 @@
-import React from 'react';
-// import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 // import { FaAngleDown } from "react-icons/fa";
 
 
 const Nav = () => {
-    return (
-        <nav className="mainmenu-nav">
-            <ul className="mainmenu">
-                {/* <li className="menu-item-has-children">
+  return (
+    <nav className="mainmenu-nav">
+      <ul className="mainmenu">
+      <li>
+          <Link to={process.env.PUBLIC_URL + "/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={process.env.PUBLIC_URL + "/services"}>Services</Link>
+        </li>
+        <li>
+          <Link to={process.env.PUBLIC_URL + "/our-projects"}>Portfolio </Link>
+        </li>
+        <li>
+          <Link to={process.env.PUBLIC_URL + "/team"}>Team</Link>
+        </li>
+        <li>
+          <Link to={process.env.PUBLIC_URL + "/contact"}>Contact</Link>
+        </li>
+        
+
+        {/* <li className="menu-item-has-children">
                     <Link to="#">Digital Agency <FaAngleDown /> </Link>
                     <ul className="axil-submenu">
                         <li><Link to={process.env.PUBLIC_URL + "/digital-agency"}>Digital Agency</Link></li>
@@ -18,17 +35,18 @@ const Nav = () => {
                         <li><a href="https://new.axilthemes.com/demo/react/abstrak-rtl/">RTL Demo</a></li>
                     </ul>
                 </li> */}
-                <li className="menu-item-has-children">
-                    {/* <Link to={process.env.PUBLIC_URL + "/service-two"}>Services</Link> */}
-                    {/* <ul className="axil-submenu">
+
+        {/* <li className="menu-item-has-children"> */}
+        {/* <Link to={process.env.PUBLIC_URL + "/service-two"}>Services</Link> */}
+        {/* <ul className="axil-submenu">
                         <li><Link to={process.env.PUBLIC_URL + "/service-one"}>Service</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/service-two"}>Service Two</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/service-details/design"}>Service Details</Link></li>
                     </ul> */}
-                </li>
-                <li className="menu-item-has-children">
-                    {/* <Link to={process.env.PUBLIC_URL + "/project-grid-three"}>Portfolio </Link> */}
-                    {/* <ul className="axil-submenu">
+        {/* </li> */}
+        {/* <li className="menu-item-has-children"> */}
+        {/* <Link to={process.env.PUBLIC_URL + "/project-grid-three"}>Portfolio </Link> */}
+        {/* <ul className="axil-submenu">
                         <li><Link to={process.env.PUBLIC_URL + "/project-grid-one"}>Two Column</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/project-grid-two"}>Three Column</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/project-grid-three"}>Four Column</Link></li>
@@ -36,10 +54,10 @@ const Nav = () => {
                         <li><Link to={process.env.PUBLIC_URL + "/project-width-two"}>Four Column Width</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/project-details/plan-management"}>Portfolio Details</Link></li>
                     </ul> */}
-                </li>
-                <li className="menu-item-has-children">
-                    {/* <Link to={process.env.PUBLIC_URL + "/team"}>Team</Link> */}
-                    {/* <ul className="axil-submenu">
+        {/* </li> */}
+        {/* <li className="menu-item-has-children"> */}
+        {/* <Link to={process.env.PUBLIC_URL + "/team"}>Team</Link> */}
+        {/* <ul className="axil-submenu">
                         <li><Link to={process.env.PUBLIC_URL + "/about-us"}>About Us</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/our-office"}>Our Office</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/case-study"}>Case Study</Link></li>
@@ -53,8 +71,8 @@ const Nav = () => {
                         <li><Link to={process.env.PUBLIC_URL + "/404"}>404 Page</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/coming-soon"}>Coming Soon</Link></li>
                     </ul> */}
-                </li>
-                {/* <li className="menu-item-has-children">
+        {/* </li> */}
+        {/* <li className="menu-item-has-children">
                     <Link to="#">Blog <FaAngleDown /></Link>
                     <ul className="axil-submenu">
                         <li><Link to={process.env.PUBLIC_URL + "/blog-grid"}>Blog</Link></li>
@@ -65,10 +83,13 @@ const Nav = () => {
                         <li><Link to={process.env.PUBLIC_URL + "#"}>Quote Post</Link></li>
                     </ul>
                 </li> */}
-                {/* <li><Link to={process.env.PUBLIC_URL + "/contact"}>Contact</Link></li> */}
-            </ul>
-        </nav>
-    )
-}
+        {/* <li><Link to={process.env.PUBLIC_URL + "/contact"}>Contact</Link></li> */}
+      
+      </ul>
+
+    </nav>
+    
+  );
+};
 
 export default Nav;
