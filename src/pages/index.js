6a32ae5f7f288f0049fb8d88be1097559fa42bba
timothydutki <timart575@gmail.com>
2025-02-15 03:11:17 +0300

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const HomePage = () => {
     return (
         <React.Fragment>
             <Layout>
-                <SEO title="Buznex – Business React JS Template" />
+                <SEO title="Penda Agency | Home" />
                 <div className="wrapper">
                     <Header />
                     <IntroContainer />
